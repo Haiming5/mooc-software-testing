afa2e2ac7bfbcd7fb42972d314343f3e4e4f23ba
Haiming WU <haiminw@uci.edu>
2020-04-19 16:56:44 -0700

    Complete the test class for addFive

--- src/test/java/tudelft/gettingstarted/GettingStartedTest.java
+++ src/test/java/tudelft/gettingstarted/GettingStartedTest.java
@@ -1,27 +1,30 @@
 package tudelft.gettingstarted;
 
+import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+
 
 public class GettingStartedTest {
 
     @Test
     public void addFiveTo20() {
         int result = new GettingStarted().addFive(20);
+        int result2 = new GettingStarted().addFive(95);
         Assertions.assertEquals(25,result);
+        Assertions.assertEquals(100, result2);
     }
 
 //    UNCOMMENT THE CODE BELOW, AND FILL THE GAPS!
 
-//    @Test
-//    public void addFiveToZero() {
-//        int result = new GettingStarted().addFive(???);
-//        Assertions.assertEquals(???, result);
-//    }
+    @Test
+    public void addFiveToZero() {
+        int result = new GettingStarted().addFive(0);
+        Assertions.assertEquals(5, result);
+    }
 //
-//    @Test
-//    public void addFiveToMinus20() {
-//        int result = new GettingStarted().addFive(???);
-//        Assertions.assertEquals(????,result);
-//    }
+    @Test
+    public void addFiveToMinus20() {
+        int result = new GettingStarted().addFive(-20);
+        Assertions.assertEquals(-15,result);
+    }
 }
